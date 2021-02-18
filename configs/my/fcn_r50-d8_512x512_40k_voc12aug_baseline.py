2cf0e403c05d27641b9c35492a885b9e60a4a229
Bingyuan Liu <liubingyuan1988@gmail.com>
2021-02-18 10:37:19 -0500

    update my config

--- configs/my/fcn_r50-d8_512x512_40k_voc12aug_baseline.py
+++ configs/my/fcn_r50-d8_512x512_40k_voc12aug_baseline.py
@@ -7,19 +7,19 @@ model = dict(
     auxiliary_head=None
 )
 data = dict(
-    samples_per_gpu=4,
-    workers_per_gpu=8
+    samples_per_gpu=6,
+    workers_per_gpu=4,
 )
 # optimizer
-#optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
-optimizer = dict(type='SGD', lr=2.0e-5, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(type='SGD', lr=0.005, momentum=0.9, weight_decay=0.0005)
+# optimizer = dict(type='SGD', lr=2.0e-5, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=5e-6, by_epoch=False)
 # runtime settings
-runner = dict(type='IterBasedRunner', max_iters=20000)
+runner = dict(type='IterBasedRunner', max_iters=60000)
 checkpoint_config = dict(by_epoch=False, interval=4000)
-evaluation = dict(interval=4000, metric='mIoU')
+evaluation = dict(interval=60000, metric='mIoU')
 log_config = dict(
     interval=50,
     hooks=[
